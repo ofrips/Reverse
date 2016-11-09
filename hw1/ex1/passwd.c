dde1a1ac7b003c49e826c1bb423c457ae54a48a7
Itamar Raviv <raviv_itamar@campus.technion.ac.il>
2016-11-09 13:02:30 +0200

    fixed stackoverflow issue. finished the program

--- hw1/ex1/passwd.c
+++ hw1/ex1/passwd.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 
 int main () {
 	
-	char input[256];
+	char input[12];
 	char names[] = "itamarofri";
-	//fgets(input, sizeof(input), stdin);
-	//fflush(stdin);
-	gets(input);
+	fgets(input,12,stdin);
+	char* nl = strchr(input, '\n');
+	if (nl) *nl =0;
+	if (strlen(input)>10){
+		printf("Wrong ID\n");
+		return 0;
+	}
 	if (!strcmp(input,names)){
 			printf("OK\n");
 			return 0;
